a48d19751cf0fb9143eb73d7805cdf5de48bf916
Eric Smith <brouhaha@fedoraproject.org>
2009-05-24 18:54:21 +0000

    *** empty log message ***

--- asm_types.h
+++ asm_types.h
@@ -1,10 +1,16 @@
 /*
 Copyright 1995, 2004, 2005, 2006, 2007, 2008, 2009 Eric Smith <eric@brouhaha.com>
 All rights reserved.
-$Id$
+$Id: pasm_types.h,v 1.1 2009/05/10 00:23:51 eric Exp eric $
 */
 
 typedef uint16_t addr_t;
 
 typedef uint16_t uword_t;
 typedef int16_t  sword_t;
+
+typedef struct
+{
+  uword_t word1;
+  uword_t word2;
+} udword_t;
