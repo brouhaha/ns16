c1afb7ec0bc9d67c64db2be93594eba1f24a74ae
Eric Smith <brouhaha@fedoraproject.org>
2015-01-04 00:17:35 +0000

    *** empty log message ***

--- screenedit.py
+++ screenedit.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
-# Python 2.5 or later required
+# Python 2.6 or later required
 
-from __future__ import with_statement # not required for Python 2.6 or newer
-
+import argparse
 import os
 import sys
 import tempfile
 
-fn = 'figforth_blocks'
+default_bfn = 'forth.blks'
+default_editor = os.environ.get('VISUAL')
+if default_editor is None:
+    default_editor = os.environ.get('EDITOR')
+if default_editor is None:
+    default_editor = 'emacs'
+
 chars_per_line = 64
 lines_per_screen = 16
 chars_per_screen = chars_per_line * lines_per_screen
@@ -36,9 +41,9 @@ def replace_nonprinting_chars_with_space (buf):
     return ''.join (chars)
 
 
-def read_screen (screen_num):
-    f.seek (screen_num * chars_per_screen, os.SEEK_SET)
-    buf = f.read (chars_per_screen)
+def read_screen (bf, screen_num):
+    bf.seek (screen_num * chars_per_screen, os.SEEK_SET)
+    buf = bf.read (chars_per_screen)
 
     # fix possible problems with screen
     buf = pad_short_screen (buf)
@@ -46,9 +51,9 @@ def read_screen (screen_num):
     return buf
 
 
-def write_screen (screen_num, buf):
-    f.seek (screen_num * chars_per_screen, os.SEEK_SET)
-    f.write (buf)
+def write_screen (bf, screen_num, buf):
+    bf.seek (screen_num * chars_per_screen, os.SEEK_SET)
+    bf.write (buf)
 
 
 def write_screen_to_text_file (buf):
@@ -79,20 +84,37 @@ def read_screen_from_text_file (tfn):
     return pad_string_with_spaces (buf, chars_per_screen)
 
 
-def edit_text_file (fn):
-    os.system ('emacs ' + fn)
+def edit_text_file (fn, editor):
+    os.system (editor + ' ' + fn)
 
 
-screen_num = int (sys.argv [1])
-with open (fn, 'r+b') as f:
-    buf = read_screen (screen_num)
+parser = argparse.ArgumentParser (description = 'edit a FORTH screen')
+                    
+parser.add_argument ('screen_num', metavar = 'screen',
+                     type = int,
+                     help = 'screen number')
+
+parser.add_argument ('--editor', '-e',
+                     default = default_editor,
+                     help = 'editor program (default: %(default)s)')
+
+parser.add_argument ('--file', '-f',
+                     type = argparse.FileType('r+b'),
+                     default = default_bfn,
+                     help = 'FORTH block file (default: %(default)s)')
+
+args = parser.parse_args()
+print args
+
+with args.file as bf:
+    buf = read_screen (bf, args.screen_num)
 
     tfn = write_screen_to_text_file (buf)
-    edit_text_file (tfn)
+    edit_text_file (tfn, args.editor)
     buf2 = read_screen_from_text_file (tfn)
     os.remove (tfn)
 
     if buf == buf2:
         print "screen unchanged"
     else:
-        write_screen (screen_num, buf2)
+        write_screen (bf, args.screen_num, buf2)
